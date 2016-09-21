bb81b8911fee43f00ffbcadd4e71f87b76bbac1b
francheskas <francheskassalazar@gmail.com>
2016-09-21 13:03:10 -0700

    completed lesson 1

---
+++ Lesson_01/ASCIIART.java
@@ -0,0 +1,19 @@
+public class ASCIIART
+{
+	public static void main (String[]args)
+	{
+		System.out.println("  //");
+        System.out.println(" ('>");
+        System.out.println ("/rr");
+        System.out.println("*\\))_");
+		
+		System.out.println();
+		System.out.println();
+		
+	    System.out.println(" __    __");
+        System.out.println("/ \\\\..// \\ ");
+        System.out.println (" ( oo )  ");
+        System.out.println ("  \\__/");
+		
+	}
+}
