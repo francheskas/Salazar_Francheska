1b4738648547b8562da307ef00e2ff224848da36
francheskas <francheskassalazar@gmail.com>
2016-09-23 14:20:30 -0700

    completed lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,37 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 5;
+		int y = 6;
+		int xy = x * y;
+		
+		System.out.println(x + " " + "multiplied by" + " " + y + " " + "is" + " " + xy);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		String name = "Elizabeth Peterson";
+		String street = "Happiness Street";
+		String number = "505";
+		String city = "San Diego";
+		String state = "CA";
+		String zipcode = "92123";
+		
+		System.out.println(name);
+		System.out.println(street + " " + number);
+		System.out.println(city + "," + " " + state + " " + zipcode);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		int l = 5;
+		int w = 4;
+		int h = 7;
+		int surfaceArea = 2 * l * w + 2 * l * h + 2 * w * h;
+		
+		System.out.println("The surface area of your rectangle is" + " " + surfaceArea);
+	}
+}
