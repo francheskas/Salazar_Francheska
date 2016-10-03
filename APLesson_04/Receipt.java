b2343c9775c2c8b28c5daa5356ad9bdac65ff7fa
francheskas <francheskassalazar@gmail.com>
2016-10-03 14:37:40 -0700

    Started Lesson_04

---
+++ APLesson_04/Receipt.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Receipt
+{
+	public static void main (String[]args)
+	{
+		Scanner keyborad = new Scanner (System.in);
+		
+		System.out.println("Please enter item 1:");
+		
+		
+		
+	}
+}
