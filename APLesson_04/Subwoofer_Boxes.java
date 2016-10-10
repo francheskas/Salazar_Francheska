fb7e744efe43f6421db969570fa89ea16e56ea5d
francheskas <francheskassalazar@gmail.com>
2016-10-10 11:45:48 -0700

    completed lesson 4.1

---
+++ APLesson_04/Subwoofer_Boxes.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Subwoofer_Boxes
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner (System.in);
+		
+		System.out.println("Enter the height in inches:");
+		double height = keyboard.nextDouble();
+		
+		System.out.println("Enter the lenght in inches:");
+		double length = keyboard.nextDouble();
+		
+		System.out.println("Enter the width in inches:");
+		double width = keyboard.nextDouble();
+		
+		Subwoofer_Boxes calVol = new Subwoofer_Boxes();
+		System.out.println("Your volume is " + calcVol(height, length, width) + " ft.");
+		
+		
+	}
+	
+	public static double calcVol(double h, double l, double w)
+	{
+		return ((h*l*w)/1728);
+	}
+}
