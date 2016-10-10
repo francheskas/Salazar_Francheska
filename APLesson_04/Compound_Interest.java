0977f90ccb7160b0b458af197d5d904cdd2233ef
francheskas <francheskassalazar@gmail.com>
2016-10-10 11:11:37 -0700

    completed lesson 4.1

--- APLesson_04/Compound_Interest.java
+++ APLesson_04/Compound_Interest.java
@@ -7,16 +7,25 @@ public class Compound_Interest
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("Enter the interest rate:");
-		double r = keyboard.nextDouble();
+		double rate = keyboard.nextDouble();
 		
 		System.out.println("Enter the amount you inetend to borrow:");
-		double P = keyboard.nextDouble();
+		double principal = keyboard.nextDouble();
 		
 		System.out.println("Enter the number of times the loan is compounded per year:");
-		int n = keyboard.nextInt();
+		double number = keyboard.nextDouble();
 	
-		System.out.println("Enter the life of the loan in year:");
-		int t = keyboard.nextInt();
+		System.out.println("Enter the life of the loan in years:");
+		double time = keyboard.nextDouble();
 		
+		
+		Compound_Interest payment = new Compound_Interest();
+		System.out.println("Your monthly payment will be " + payment(rate, principal, number, time));
+	}
+	
+	public static double payment(double r, double p, double n, double t)
+	{
+		 return(p *(Math.pow(1+(r/n), n*t)));
+		 
 	}
 }
