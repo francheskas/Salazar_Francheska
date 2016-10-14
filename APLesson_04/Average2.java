df0617e4a98be8905c6825d8bebe9e5da1fc0d26
francheskas <francheskassalazar@gmail.com>
2016-10-14 11:58:45 -0700

    completed exercise 2

---
+++ APLesson_04/Average2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Average2
+{
+	public static void main (String[]args)
+	
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter your first number:");
+		double num1 = keyboard.nextDouble();
+		
+		System.out.println("Enter your second number:");
+		double num2 = keyboard.nextDouble();
+		
+		System.out.println("Enter your third number:");
+		double num3 = keyboard.nextDouble();
+		
+		double average = calcAvg(num1, num2, num3);
+		
+		System.out.printf("The average of " + num1 + ", " + num2 +", and "  + num3 + " is %5.5f .", average);
+	}
+	
+	public static double calcAvg(double num1, double num2, double num3)
+	{
+		return ((num1+num2+num3)/3);
+	}
+}
